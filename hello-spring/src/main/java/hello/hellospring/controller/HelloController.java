6e57068f774d39864831b45e06bfe1bbd5894ac6
Son-Gyeongi <thsruddl77@gmail.com>
2025-10-28 20:04:48 +0900

    section2 5. View 환경설정

---
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,15 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller // 웹 어플리케이션에서 첫 번째 진입점이 컨트롤러이다.
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "hello"; // templates/hello.html
+    }
+}
