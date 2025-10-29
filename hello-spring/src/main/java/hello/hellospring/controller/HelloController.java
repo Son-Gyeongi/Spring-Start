ea882588653e578ba93383b97ae61a50d18207cb
Son-Gyeongi <thsruddl77@gmail.com>
2025-10-29 20:53:54 +0900

    section3 9. API

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,20 +4,51 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // 웹 어플리케이션에서 첫 번째 진입점이 컨트롤러이다.
 public class HelloController {
 
+    // MVC 와 템플릿 엔진
     @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "hello"; // templates/hello.html
     }
 
+    // MVC 와 템플릿 엔진
     @GetMapping("hello-mvc")
     public String helloMvc(@RequestParam("name") String name,
                            Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    // API
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; // 문자 그대로 반환
+    }
+
+    // API
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; // 객체 반환 - json 으로 반환
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
