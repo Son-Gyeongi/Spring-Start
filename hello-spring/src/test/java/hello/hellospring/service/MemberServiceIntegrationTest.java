82aaf6079874ce0377e78ec8cbc0293b13c741b8
Son-Gyeongi <thsruddl77@gmail.com>
2025-11-05 17:32:26 +0900

    section7 22. 스프링 통합 테스트

---
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,57 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+// 스프링 통합 테스트
+@SpringBootTest
+@Transactional // 테스트는 반복할 수 있어야 한다.
+class MemberServiceIntegrationTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    // 정상 플로어
+    @Test
+    void 회원가입() {
+        // given - 주어지는 것
+        Member member = new Member();
+        member.setName("spring");
+
+        // when - 검증할 로직
+        Long saveId = memberService.join(member);
+
+        // then - 검증
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    // 예외 플로어
+    // 중복 회원 검증 로직으로 예외 터지는 것도 봐야함
+    @Test
+    public void 중복_회원_예외() {
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // when
+        memberService.join(member1);
+        // memberService.join(member2) 로직 실행하면 IllegalStateException.class 예외가 터짐
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+    }
+}
